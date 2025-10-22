46f28049efa90d39c7d83a648f3b69128598cdfc
HyunjoonCho <jhj09016@gmail.com>
2025-10-22 16:58:44 +0900

    replace HF api with HF transformers

--- scripts/llm_api.py
+++ scripts/llm_api.py
@@ -4,115 +4,38 @@ import requests
 import random
 
 import openai
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, StoppingCriteria, StoppingCriteriaList
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-HF_KEY = os.getenv("HF_API_KEY")
+client = openai.OpenAI(api_key=openai.api_key)
+HF_KEY = os.getenv("HUGGINGFACE_TOKEN")
 
 AVAILABLE_MODEL_INFO = {
-    'OpenAI/text-curie-001': {
-        'query_type': 'openai',
-        'uses_chat': False,
-    },
-    'OpenAI/text-davinci-002': {
-        'query_type': 'openai',
-        'uses_chat': False,
-    },
-    'OpenAI/text-davinci-003': {
-        'query_type': 'openai',
-        'uses_chat': False,
-    },
-    'OpenAI/gpt-3.5-turbo': {
+    'OpenAI/gpt-4o': {
         'query_type': 'openai',
         'uses_chat': True,
     },
-    'OpenAI/gpt-4': {
-        'query_type': 'openai',
+    'meta-llama/Meta-Llama-3-8B-Instruct': {
+        'query_type': 'hf_transformers',
         'uses_chat': True,
     },
-    'bigscience/bloom': {
-        'query_type': 'hf_hosted',
-        'uses_chat': False,
-    },
-    'bigscience/bloom-7b1': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'bigscience/bloom-3b': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'bigscience/bloomz': {
-        'query_type': 'hf_hosted',
-        'uses_chat': False,
-    },
-    'bigscience/bloomz-7b1': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'bigscience/bloomz-3b': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'facebook/incoder-6B': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'facebook/incoder-1B': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'Salesforce/codegen-16B-multi': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'Salesforce/codegen-6B-multi': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'Salesforce/codegen-2B-multi': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'Salesforce/codegen-350M-multi': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'EleutherAI/gpt-neox-20b': {
-        'query_type': 'hf_hosted',
-        'uses_chat': False,
-    },
-    'EleutherAI/gpt-neo-2.7b': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'EleutherAI/gpt-neo-1.3b': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'EleutherAI/gpt-neo-125m': {
-        'query_type': 'huggingface',
-        'uses_chat': False,
-    },
-    'Databricks/dolly-v2-12b': {
-        'query_type': 'unknown',
-        'uses_chat': False,
-    },
-    'BlinkDL/rwkv-4-pile-14b': {
-        'query_type': 'unknown',
-        'uses_chat': False,
-    },
-    'BlinkDL/rwkv-4-pile-7b': {
-        'query_type': 'unknown',
-        'uses_chat': False,
-    },
-    'BlinkDL/rwkv-4-raven': {
-        'query_type': 'unknown',
+    'meta-llama/Meta-Llama-3.1-8B-Instruct': {
+        'query_type': 'hf_transformers',
         'uses_chat': True,
     },
-    'Salesforce/codegen2-16B': {
-        'query_type': 'self_hosted',
-        'uses_chat': False,
-    }
+    'mistralai/Mistral-Nemo-Instruct-2407': {
+        'query_type': 'hf_transformers',
+        'uses_chat': True,
+    },
+    'Qwen/Qwen2.5-Coder-7B-Instruct': {
+        'query_type': 'hf_transformers',
+        'uses_chat': True,
+    },
+    'microsoft/phi-4': {
+        'query_type': 'hf_transformers',
+        'uses_chat': True,
+    },
 }
 AVAILABLE_MODELS = AVAILABLE_MODEL_INFO.keys() # just for clean code
 
@@ -126,40 +49,37 @@ def model_is_chat(model):
 def tiny_noise(scale=1/1000):
     return scale*random.random()-0.5*scale
 
-# Query functions
-def query_hf_hosted_llm(prompt, model, stop_tokens, use_cache=False, end_len=1000*4):
-    def single_query(wip_prompt):
-        use_temp = TEMP if use_cache else (TEMP + tiny_noise())
-        data = json.dumps({'inputs': wip_prompt, 'parameters': {'temperature': use_temp}})
-        API_URL = f"https://api-inference.huggingface.co/models/{model}"
-        headers = {"Authorization": f"Bearer {HF_KEY}", "Content-Type": "application/json"}
-        response = requests.request("POST", API_URL, headers=headers, data=data)
-        assert response.status_code == 200, f'Response status was non-normal ({response.status_code}): {response.content}'
-        return json.loads(response.content.decode("utf-8"))[0]['generated_text']
+class StopOnTokens(StoppingCriteria):
+    def __init__(self, stop_strings, tokenizer):
+        self.stop_strings = stop_strings
+        self.tokenizer = tokenizer
     
-    new_content = ''
-    while (all(t not in new_content for t in stop_tokens) and 
-           len(new_content) < end_len):
-        full_gen_text = single_query(prompt + new_content)
-        if full_gen_text == prompt+new_content: # predicting end token
-            return new_content
-        new_content = full_gen_text[len(prompt):]
-    earliest_stop_loc = min([len(new_content)] + 
-                            [new_content.index(t) for t in stop_tokens if new_content.index(t) >= 0])
-    return new_content[:earliest_stop_loc]
+    def __call__(self, input_ids, scores, **kwargs):
+       decoded = self.tokenizer.decode(input_ids[0], skip_special_tokens=True)
+       return any(stop in decoded for stop in self.stop_strings)
 
-def query_self_hosted_llm(prompt, stop_tokens, max_tokens=256):
-    payload = {
-        'text': prompt,
-        'max_new_tokens': max_tokens,
-    }
-    json_payload = json.dumps(payload)
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post('http://localhost:23623', data=json_payload, headers=headers)
-    gen_content = response.json()['result']
-    earliest_stop_loc = min([len(gen_content)] +
-                            [gen_content.index(t) for t in stop_tokens if t in gen_content])
-    return gen_content[:earliest_stop_loc]
+# Query functions
+class ModelWrapper:
+    _model_name = None
+    _model = None
+    _tokenizer = None
+    _generator = None
+    
+    @classmethod
+    def generate(cls, prompt, model_name, stop_tokens, use_cache=False):
+        if not cls._generator or cls._model_name != model_name:
+            cls._model_name = model_name
+            cls._model = AutoModelForCausalLM.from_pretrained(model_name, token=HF_KEY, torch_dtype=torch.bfloat16, trust_remote_code=True, device_map='auto')
+            cls._tokenizer = AutoTokenizer.from_pretrained(model_name, token=HF_KEY, trust_remote_code=True)
+            cls._generator = pipeline("text-generation",model=cls._model, tokenizer=cls._tokenizer, torch_dtype=torch.bfloat16, device_map='auto', token=HF_KEY)
+
+        use_temp = TEMP if use_cache else (TEMP + tiny_noise())
+        prompt = cls._generator.tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
+        stopping_criteria = None
+        if stop_tokens:
+            stopping_criteria = StoppingCriteriaList([StopOnTokens(stop_tokens, cls._tokenizer)])
+                
+        return cls._generator(prompt, temperature=use_temp, stopping_criteria=stopping_criteria, return_full_text=False)[0]['generated_text']
 
 def query_chat_llm(prompt, model, stop_tokens):
     assert model in AVAILABLE_MODEL_INFO, f'Unknown model {model}'
@@ -169,7 +89,7 @@ def query_chat_llm(prompt, model, stop_tokens):
 
     if model_info['query_type'] == 'openai':
         base_model_name = model.split('/')[-1]
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model=base_model_name,
             messages=prompt, # chat-style prompt
             n=1,
@@ -177,12 +97,14 @@ def query_chat_llm(prompt, model, stop_tokens):
             presence_penalty=0,
             stop=stop_tokens
         )
-        gen_result = response["choices"][0]["message"]["content"]
-        if "```" in gen_result:
-            gen_result = gen_result.split("```")[1]
-            gen_result = gen_result.removeprefix('java')
-    elif model_info['query_type'] == 'hf_hosted':
-        raise NotImplementedError
+        gen_result = response.choices[0].message.content
+        if gen_result and "```" in gen_result:
+            parts = gen_result.split("```")
+            if len(parts) >= 2:
+                gen_result = parts[1]
+                gen_result = gen_result.removeprefix('java')
+    elif model_info['query_type'] == 'hf_transformers':
+        gen_result = ModelWrapper.generate(prompt, model, stop_tokens)
     else:
         raise NotImplementedError(f'Unknown query type {model_info["query_type"]}')
     return gen_result
@@ -193,26 +115,7 @@ def query_string_llm(prompt, model, stop_tokens):
     assert type(prompt) == str
     assert not model_info['uses_chat']
 
-    if model_info['query_type'] == 'openai':
-        base_model_name = model.split('/')[-1]
-        response = openai.Completion.create(
-            engine=base_model_name,
-            prompt=prompt,
-            temperature=0.7,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=stop_tokens
-        )
-        gen_result = response['choices'][0]['text']
-    elif model_info['query_type'] == 'hf_hosted':
-        gen_result = query_hf_hosted_llm(prompt, model, stop_tokens)
-    elif model_info['query_type'] == 'self_hosted':
-        gen_result = query_self_hosted_llm(prompt, stop_tokens, max_tokens=256)
-    else:
-        raise NotImplementedError(f'Unknown query type {model_info["query_type"]}')
-    return gen_result
+    return ""
 
 def query_llm(prompt, model, stop_tokens):
     # sanity checks
