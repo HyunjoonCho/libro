77fffb5dd0c9ad66a3730a841b4486846a59be89
HyunjoonCho <jhj09016@gmail.com>
2025-10-29 10:30:02 +0900

    rename qwen rank files for consistency

--- scripts/rank.sh
+++ scripts/rank.sh
@@ -4,5 +4,5 @@ python selection_and_ranking.py -d Defects4J -f ../results/gpt-4o_n10.json -g ..
 python selection_and_ranking.py -d Defects4J -f ../results/llama3-8b_n10.json -g ../data/Defects4J/gen_tests_Meta-Llama-3-8B-Instruct -p _llama3-8b
 python selection_and_ranking.py -d Defects4J -f ../results/llama3.1-8b_n10.json -g ../data/Defects4J/gen_tests_Meta-Llama-3.1-8B-Instruct -p _llama3.1-8b
 python selection_and_ranking.py -d Defects4J -f ../results/mistral-nemo-12b_n10.json -g ../data/Defects4J/gen_tests_Mistral-Nemo-Instruct-2407 -p _mistral-nemo-12b
-python selection_and_ranking.py -d Defects4J -f ../results/qwen2.5-coder-7b_n10.json -g ../data/Defects4J/gen_tests_Qwen2.5-Coder-7B-Instruct -p qwen2.5-coder-7b
-#python selection_and_ranking.py -d Defects4J -f ../results/phi4-14b_n10.json -g ../data/Defects4J/gen_tests_phi-4 -p phi4-14b
+python selection_and_ranking.py -d Defects4J -f ../results/qwen2.5-coder-7b_n10.json -g ../data/Defects4J/gen_tests_Qwen2.5-Coder-7B-Instruct -p _qwen2.5-coder-7b
+#python selection_and_ranking.py -d Defects4J -f ../results/phi4-14b_n10.json -g ../data/Defects4J/gen_tests_phi-4 -p _phi4-14b
