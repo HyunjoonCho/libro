06e9fe940726aa591dd3a1bb8068d2eec4172282
HyunjoonCho <jhj09016@gmail.com>
2025-10-31 04:54:17 -0700

    handle JacksonDatabind ext test issue when tagging PRE

--- data/Defects4J/tag_pre_fix_compilable.sh
+++ data/Defects4J/tag_pre_fix_compilable.sh
@@ -37,10 +37,11 @@ for bname in $(find . -mindepth 1 -maxdepth 1 -type d | sort -V); do
 
 	# attempt to fix
 	if [[ ${bname} == *"Jackson"* ]]; then
-                for fname in $(git diff D4J_${bname}_BUGGY_VERSION --name-only | grep "PackageVersion"); do
-                        git checkout D4J_${bname}_BUGGY_VERSION -- ${fname}
-                done
-        fi
+        for fname in $(git diff D4J_${bname}_BUGGY_VERSION --name-only | grep "PackageVersion"); do
+            git checkout D4J_${bname}_BUGGY_VERSION -- ${fname}
+        done
+        rm src/test/java/com/fasterxml/jackson/databind/ext/TestSOAP.java
+    fi
 	if [[ ${bname} == *"Time"* ]] && (( $(ls -1 | wc -l) < 5 )); then
 		mv defects4j.build.properties JodaTime
 		mv .defects4j.config JodaTime
@@ -75,7 +76,7 @@ for bname in $(find . -mindepth 1 -maxdepth 1 -type d | sort -V); do
 	if [[ $? -eq 0 ]]; then
 		echo "${bname},ok"
 		git commit -m "D4J_${bname}_PRE_FIX_COMPILABLE"
-                git tag D4J_${bname}_PRE_FIX_COMPILABLE
+        git tag D4J_${bname}_PRE_FIX_COMPILABLE
 	else
 		echo "${bname},fail"
 	fi
